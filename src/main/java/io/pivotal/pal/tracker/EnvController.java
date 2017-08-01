2ebc71d8d0306039498811d2fea90f335eced278
accenturelabs <accenturelabs@accenturelabss-iMac.local>
2017-08-01 09:25:23 -0400

    concourse

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,48 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by accenturelabs on 7/31/17.
+ */
+@RestController
+public class EnvController {
+
+
+    private String port;
+
+    private String memoryLimit;
+
+    private String cfInstanceIndex;
+
+    private String cfInstanceAddress;
+
+    private Map<String, String> env = new HashMap<>();
+
+
+    public EnvController(@Value("${PORT:NOT SET}")String port, @Value("${MEMORY_LIMIT:NOT SET}")String memoryLimit, @Value("${CF_INSTANCE_INDEX:NOT SET}")String cfInstanceIndex, @Value("${CF_INSTANCE_ADDR:NOT SET}")String cfInstanceAddress){
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddress = cfInstanceAddress;
+
+
+    }
+
+    @GetMapping("/env")
+    public Map getEnv(){
+
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddress);
+
+        return env;
+    }
+
+}
