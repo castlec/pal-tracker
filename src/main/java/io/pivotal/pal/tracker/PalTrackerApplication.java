a0c9c15d5d0beab86cde676353e7fab7d61e9d8e
castle <onyxrook@gmail.com>
2017-08-01 16:56:01 -0400

    jdbc

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,18 +1,30 @@
 package io.pivotal.pal.tracker;
 
+import org.mariadb.jdbc.MariaDbDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+
 @SpringBootApplication
 public class PalTrackerApplication {
+
+    @Autowired
+    private DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
+
+
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository() throws SQLException {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
